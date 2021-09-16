eabc7799f3b99ee4a0f70d87b0095a9cf71355f4
Paul Adams <ghusiyy@gmail.com>
2021-09-16 12:32:55 -0700

    impl for longest common prefix

--- strings/index.test.ts
+++ strings/index.test.ts
@@ -14,6 +14,7 @@ Deno.test("valid parentheses", () => {
 });
 
 Deno.test("Longest common prefix", () => {
+  assertEquals("aar", longestCommonPrefix(["aar", "aar", "aarm"]));
   assertEquals("fl", longestCommonPrefix(["flower", "flow", "flight"]));
   assertEquals("", longestCommonPrefix(["dog", "racecar", "car"]));
 });
--- strings/index.ts
+++ strings/index.ts
@@ -15,6 +15,21 @@ export function validParens(p: string): boolean {
   return !seen.length;
 }
 
+function matchingPrefix(s1: string, s2: string) {
+  let match = "";
+  for (let i = 0, j = 0; i <= s1.length - 1 && j <= s2.length - 1; i++, j++) {
+    if (s1[i] != s2[j]) {
+      break;
+    }
+    match += s1[i];
+  }
+  return match;
+}
+
 export function longestCommonPrefix(strs: string[]) {
-  return "";
+  let result = strs[0];
+  for (let i = 1; i <= strs.length - 1; i++) {
+    result = matchingPrefix(result, strs[i]);
+  }
+  return result;
 }
