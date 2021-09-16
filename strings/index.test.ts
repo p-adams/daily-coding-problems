7d75107524dbf7dd6a76df344183f1617dd7dde4
Paul Adams <ghusiyy@gmail.com>
2021-09-15 21:39:16 -0700

    impl for validParens

--- strings/index.test.ts
+++ strings/index.test.ts
@@ -2,5 +2,9 @@ import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 import { validParens } from "./index.ts";
 
 Deno.test("valid parentheses", () => {
+  assertEquals(true, validParens(""));
+  assertEquals(true, validParens("()"));
   assertEquals(false, validParens("("));
+  assertEquals(false, validParens(")"));
+  assertEquals(true, validParens("(())"));
 });
--- strings/index.ts
+++ strings/index.ts
@@ -1,9 +1,12 @@
 export function validParens(p: string): boolean {
   const seen: string[] = [];
   for (const ch of p) {
+    const top = seen[seen.length - 1];
     if (ch === "(") {
       seen.push(ch);
-    }
+    } else if (ch === ")" && top === "(") {
+      seen.pop();
+    } else return false;
   }
   return !seen.length;
 }
