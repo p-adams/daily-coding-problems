b63cf6227507ede3f36935bb228be280740f58af
Paul Adams <ghusiyy@gmail.com>
2021-09-24 16:45:32 -0700

    update with comments

--- misc/slidingWindow.js
+++ misc/slidingWindow.js
@@ -1,4 +1,20 @@
 // sliding window algorithms
-export function distinctElements(s, k) {
-  return 2 + 2;
+// underscore prefix denotes brute force solution
+export function __maxSumOfKConsecutiveElements(nums, k) {
+  // input = [100, 200, 300, 400]
+  // k = 2
+  // output = 700
+  if (k > nums.length) {
+    return 0;
+  }
+  let maxSum = -Infinity;
+  let currSum;
+  for (let i = 0; i <= nums.length - k; i++) {
+    currSum = 0;
+    for (let j = i; j < i + k; j++) {
+      currSum += nums[j];
+    }
+    maxSum = Math.max(maxSum, currSum);
+  }
+  return maxSum;
 }
--- misc/slidingWindow.test.js
+++ misc/slidingWindow.test.js
@@ -1,6 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 import * as sw from "./slidingWindow.js";
 
-Deno.test("longest substring containing k distinct elements", () => {
-  assertEquals(4, sw.distinctElements("foobar", 2));
+Deno.test("maximum sum of k consecutive elements", () => {
+  assertEquals(0, sw.__maxSumOfKConsecutiveElements([], 2));
+  assertEquals(700, sw.__maxSumOfKConsecutiveElements([100, 200, 300, 400], 2));
+  assertEquals(
+    39,
+    sw.__maxSumOfKConsecutiveElements([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)
+  );
 });
