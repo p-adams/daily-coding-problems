04e58b5e138aa2cdf3813e2994961f62574847e9
Paul Adams <ghusiyy@gmail.com>
2021-10-04 01:24:45 -0700

    set up List class

--- lists/index.test.ts
+++ lists/index.test.ts
@@ -1,6 +1,15 @@
 import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 import * as L from "./index.ts";
 
+function populateList(size: number) {
+  const list = new L.List();
+  for (const item of [...Array(size).keys()]) {
+    list.addItem(item);
+  }
+  return list;
+}
+
 Deno.test("add new list item", () => {
-  assertEquals(2, L.addItem(2));
+  const list = populateList(2);
+  assertEquals(2, list.length());
 });
--- lists/index.ts
+++ lists/index.ts
@@ -1,1 +1,29 @@
-export function addItem(item: number) {}
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+  length(): number {
+    if (!this.next) {
+      return 1;
+    }
+    return this.next.length() + 1;
+  }
+}
+
+export class List {
+  head?: ListNode;
+
+  addItem(item: number) {
+    const prevHead: ListNode | undefined = this.head;
+    this.head = new ListNode(item, prevHead);
+  }
+  length() {
+    return this.head?.length();
+  }
+  hasCycle(): boolean {
+    return false;
+  }
+}
