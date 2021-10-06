1673ece9b9ee20118ebd14858a8a7f8f2a1c7182
Paul Adams <ghusiyy@gmail.com>
2021-10-05 19:36:51 -0700

    modular implementation

--- lists/index.ts
+++ lists/index.ts
@@ -67,3 +67,17 @@ export class List {
     return result;
   }
 }
+function mergeTwoLists2(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  if (!l2) return l1;
+  if (!l1) return l2;
+  if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists2(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists2(l1, l2.next);
+    return l2;
+  }
+}
