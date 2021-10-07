0541bfac5152ade1d273ff44a81f00457107a61c
Paul Adams <ghusiyy@gmail.com>
2021-10-07 16:58:57 -0700

    impl for MinStack problem

--- stacks/index.test.ts
+++ stacks/index.test.ts
@@ -1,5 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
-
+import { MinStack } from "./index.ts";
 Deno.test("MinStack class operations", () => {
-  assertEquals(0, 2 - 2);
+  const minStack = new MinStack();
+  minStack.push(-2);
+  minStack.push(0);
+  minStack.push(-3);
+  assertEquals(-3, minStack.getMin());
+  minStack.pop();
+  assertEquals(0, minStack.top());
+  assertEquals(-2, minStack.getMin());
 });
--- stacks/index.ts
+++ stacks/index.ts
@@ -1,1 +1,23 @@
-export class MinStack {}
+export class MinStack {
+  private _stack: number[];
+  constructor() {
+    this._stack = [];
+  }
+  push(val: number): void {
+    this._stack.push(val);
+  }
+  pop(): void {
+    this._stack.pop();
+  }
+  top(): number {
+    const last = this._stack[this._stack.length - 1];
+    return last;
+  }
+  getMin(): number {
+    let min = this._stack[0];
+    for (let i = 1; i < this._stack.length; i++) {
+      min = Math.min(min, this._stack[i]);
+    }
+    return min;
+  }
+}
