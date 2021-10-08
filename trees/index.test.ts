60581a4ea26d8fa7adb54da5d23f486ff8976347
Paul Adams <ghusiyy@gmail.com>
2021-10-07 17:10:09 -0700

    add tree module and Trie class to faciliate solving prefix tree problem

---
+++ trees/index.test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+import { Trie } from "./index.ts";
+
+Deno.test("prefix tree", () => {
+  const trie = new Trie();
+  trie.insert("apple");
+  assertEquals(true, trie.search("apple"));
+  assertEquals(false, trie.search("app"));
+  assertEquals(true, trie.startsWith("app"));
+  trie.insert("app");
+  assertEquals(true, trie.search("app"));
+});
---
+++ trees/index.ts
@@ -0,0 +1,12 @@
+export class Trie {
+  constructor() {}
+  insert(word: string): void {}
+  // checks if 'word' is in trie
+  search(word: string): boolean {
+    return false;
+  }
+  // checks if word in tree has given prefix
+  startsWith(prefix: string): boolean {
+    return false;
+  }
+}
