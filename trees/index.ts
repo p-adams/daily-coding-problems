5f9435f6e0462b2b02fec9209ac1ba3959f47b77
Paul Adams <ghusiyy@gmail.com>
2021-10-07 20:50:23 -0700

    impl for Prefix Tree

--- trees/index.ts
+++ trees/index.ts
@@ -1,12 +1,64 @@
+class TrieNode {
+  children: Map<string, TrieNode>;
+  isEnd: boolean;
+  constructor() {
+    this.isEnd = false;
+    this.children = new Map();
+  }
+}
+
 export class Trie {
-  constructor() {}
-  insert(word: string): void {}
+  root: TrieNode;
+  constructor() {
+    this.root = new TrieNode();
+  }
+  insert(word: string): void {
+    this.insertIntoTrie(word, 0, this.root);
+  }
+  insertIntoTrie(word: string, index: number, node: TrieNode | undefined) {
+    const ch = word.charAt(index);
+    const children = node!.children;
+    if (!children.has(ch)) {
+      children.set(ch, new TrieNode());
+    }
+    if (index === word.length - 1) {
+      children.get(ch)!.isEnd = true;
+    } else {
+      this.insertIntoTrie(word, index + 1, node!.children.get(ch));
+    }
+  }
   // checks if 'word' is in trie
   search(word: string): boolean {
-    return false;
+    return this.wordSearch(word, 0, this.root);
+  }
+  wordSearch(word: string, index: number, node: TrieNode | undefined): boolean {
+    const ch = word.charAt(index);
+    const children = node!.children;
+    if (!children.has(ch)) {
+      return false;
+    }
+    if (index === word.length - 1) {
+      return children.get(ch)!.isEnd;
+    }
+    return this.wordSearch(word, index + 1, children.get(ch));
   }
   // checks if word in tree has given prefix
   startsWith(prefix: string): boolean {
-    return false;
+    return this.wordStartsWith(prefix, 0, this.root);
+  }
+  wordStartsWith(
+    prefix: string,
+    index: number,
+    node: TrieNode | undefined
+  ): boolean {
+    const ch = prefix.charAt(index);
+    const children = node!.children;
+    if (!children.has(ch)) {
+      return false;
+    }
+    if (index === prefix.length - 1) {
+      return true;
+    }
+    return this.wordStartsWith(prefix, index + 1, children.get(ch));
   }
 }
