6aea1beea4d6ddcf3e9a0d4014a12ef2e4e837f4
Paul Adams <ghusiyy@gmail.com>
2021-10-28 23:34:37 -0700

    init new module for Algorithm 1 SP

---
+++ sp_algorithm1/index.js
@@ -0,0 +1,10 @@
+// Day 1: Binary Search
+function binarySearch() {}
+function firstBadVersion() {}
+function searchInsertPosition() {}
+
+export const Day1 = {
+  binarySearch,
+  firstBadVersion,
+  searchInsertPosition,
+};
---
+++ sp_algorithm1/index.test.js
@@ -0,0 +1,5 @@
+import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+import { Day1 } from "./index.js";
+Deno.test("Day 1: Binary Search", () => {
+  assertEquals(4, 2 + 2);
+});
