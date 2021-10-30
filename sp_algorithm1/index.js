540c30b0e463fa251fec2a8d04a92ca081854eb7
Paul Adams <ghusiyy@gmail.com>
2021-10-29 20:08:36 -0700

    iterative and recursive binary search

--- sp_algorithm1/index.js
+++ sp_algorithm1/index.js
@@ -1,10 +1,41 @@
 // Day 1: Binary Search
-function binarySearch() {}
+function binarySearch(nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
+}
+function binarySearchHelper(nums, start, end, target) {
+  if (end >= start) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      return binarySearchHelper(nums, start, mid - 1, target);
+    } else {
+      return binarySearchHelper(nums, mid + 1, end, target);
+    }
+  }
+  return -1;
+}
+function binarySearch2(nums, target) {
+  return binarySearchHelper(nums, 0, nums.length - 1, target);
+}
 function firstBadVersion() {}
 function searchInsertPosition() {}
 
 export const Day1 = {
   binarySearch,
+  binarySearch2,
   firstBadVersion,
   searchInsertPosition,
 };
--- sp_algorithm1/index.test.js
+++ sp_algorithm1/index.test.js
@@ -1,5 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 import { Day1 } from "./index.js";
 Deno.test("Day 1: Binary Search", () => {
-  assertEquals(4, 2 + 2);
+  // iterative
+  assertEquals(4, Day1.binarySearch([-1, 0, 3, 5, 9, 12], 9));
+  assertEquals(-1, Day1.binarySearch([-1, 0, 3, 5, 9, 12], 2));
+  // recursive
+  assertEquals(4, Day1.binarySearch2([-1, 0, 3, 5, 9, 12], 9));
+  assertEquals(-1, Day1.binarySearch2([-1, 0, 3, 5, 9, 12], 2));
 });
