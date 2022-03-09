99464763f2f7b07bb75d35f0ef399592b2ad5a33
Paul Adams <ghusiyy@gmail.com>
2022-03-08 18:25:41 -0800

    build array

--- arrays/index.ts
+++ arrays/index.ts
@@ -657,3 +657,12 @@ export function hasDistinctIndices(nums: number[], k: number): boolean {
   }
   return false;
 }
+
+function _buildArray(nums: number[]): number[] {
+  const ans = [];
+  for (let i = 0; i < nums.length; ++i) {
+    ans[i] = nums[nums[i]];
+  }
+
+  return ans;
+}
