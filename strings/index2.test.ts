ec35bb1d89b8480f9d8fe9ba1420c058d1d2ba41
Paul Adams <ghusiyy@gmail.com>
2022-03-14 00:18:52 -0700

    truncate sentence solution

--- strings/index2.test.ts
+++ strings/index2.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 import * as string from "./index2.ts";
 Deno.test("truncate sentence", () => {
   assertEquals(
-    "Hello how are you",
-    string.truncateSentence("Hello how are you Contestant", 4)
+    "The quick brown fox",
+    string.truncateSentence("The quick brown fox jumps over the lazy dog", 4)
   );
 });
--- strings/index2.ts
+++ strings/index2.ts
@@ -1,3 +1,3 @@
 export function truncateSentence(s: string, k: number): string {
-  return s;
+  return s.split(" ").slice(0, k).join(" ");
 }
