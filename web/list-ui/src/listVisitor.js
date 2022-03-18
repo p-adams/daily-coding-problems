2b8704a2c19436f4b16d3846834e5830769bce3b
Paul Adams <ghusiyy@gmail.com>
2022-03-17 22:41:58 -0700

    reorganize web project

--- web/list-ui/src/listVisitor.js
+++ web/list-ui/src/listVisitor.js
@@ -1,5 +1,3 @@
-import "./style.css";
-
 /** TODO:
  * Create a function that, given a DOM Element on the page, will visit the element itself and all of its descendents (not just its immediate children). For each element visited, the function should pass that element to a provided callback function.
 
@@ -9,7 +7,3 @@ import "./style.css";
     a callback function (that takes a DOM element as its argument)
  * 
  */
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
