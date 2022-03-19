bed580cca5355b32fe3acd0b8d9ab188a270502c
Paul Adams <ghusiyy@gmail.com>
2022-03-19 02:48:25 -0700

    visit all DOM elements on page

--- web/list-ui/src/main.js
+++ web/list-ui/src/main.js
@@ -1,3 +1,22 @@
 import "../style.css";
 
-const app = document.querySelector("#app");
+function visit(node, fn) {
+  fn(node);
+  if (node.firstChild) {
+    visit(node.firstChild, fn);
+  }
+  if (node.nextSibling) {
+    visit(node.nextSibling, fn);
+  }
+}
+
+function main() {
+  const app = document.querySelector("#app");
+  if (app) {
+    visit(app, (node) => {
+      console.log("visited: ", node);
+    });
+  }
+}
+
+main();
