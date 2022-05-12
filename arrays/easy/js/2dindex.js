f7caf80d66e780f697ebb96a6eeca6428140033e
Paul Adams <ghusiyy@gmail.com>
2022-05-12 14:37:45 -0700

    projection area solution

--- arrays/easy/js/2dindex.js
+++ arrays/easy/js/2dindex.js
@@ -53,3 +53,22 @@ export function luckyNumbers(matrix) {
 
   return [];
 }
+
+function _projectionArea(grid) {
+  const maxs = new Array(grid.length).fill(0);
+
+  grid.forEach((row) =>
+    row.forEach((val, idx) => {
+      if (maxs[idx] < val) maxs[idx] = val;
+    })
+  );
+
+  const z = grid.reduce(
+    (prev, curr) => prev + curr.filter((val) => val !== 0).length,
+    0
+  );
+  const y = grid.reduce((prev, curr) => prev + Math.max(...curr), 0);
+  const x = maxs.reduce((prev, curr) => prev + curr, 0);
+
+  return x + y + z;
+}
